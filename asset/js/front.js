6380f07d400ac477105db836b48a7cc516fb2415
voltan <djvoltan@gmail.com>
2016-11-18 09:08:08 +0330

    Import theme

---
+++ asset/js/front.js
@@ -0,0 +1,56 @@
+(function($) {
+    $(document).ready(function () {
+        $('#pi-system .pi-zone-7 .owl-carousel').owlCarousel({
+            margin: 3,
+            nav: true,
+            rtl: true,
+            autoplay: true,
+            dots: false,
+            autoplayTimeout: 12000,
+            autoplayHoverPause: true,
+            navText: ['<i class="owl-prev fa fa-angle-left"></i>', '<i class="owl-next fa fa-angle-right"></i>'],
+            responsive: {
+                0: {items: 2},
+                400: {items: 3},
+                600: {items: 4},
+                1000: {items: 5}
+            }
+        })
+    });
+    $(document).ready(function () {
+        $('#pi-system .pi-zone-6 .owl-carousel').owlCarousel({
+            margin: 3,
+            nav: true,
+            rtl: true,
+            autoplay: true,
+            dots: false,
+            autoplayTimeout: 12000,
+            autoplayHoverPause: true,
+            navText: ['<i class="owl-prev fa fa-angle-left"></i>', '<i class="owl-next fa fa-angle-right"></i>'],
+            responsive: {
+                0: {items: 1},
+                400: {items: 2},
+                600: {items: 2},
+                1000: {items: 3}
+            }
+        })
+    });
+    $(document).ready(function () {
+        $('#pi-system .pi-zone-5 .owl-carousel').owlCarousel({
+            margin: 3,
+            nav: true,
+            rtl: true,
+            autoplay: true,
+            dots: false,
+            autoplayTimeout: 12000,
+            autoplayHoverPause: true,
+            navText: ['<i class="owl-prev fa fa-angle-left"></i>', '<i class="owl-next fa fa-angle-right"></i>'],
+            responsive: {
+                0: {items: 1},
+                400: {items: 2},
+                600: {items: 2},
+                1000: {items: 3}
+            }
+        })
+    });
+})(jQuery)
